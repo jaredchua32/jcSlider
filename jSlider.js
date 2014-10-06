3d7cb7d04ce33032098e960e7675aa0116bf5698
Jared Chua <jaredchua32@gmail.com>
2014-10-06 11:28:09 -0700

    Fixed the pausing of the slider between slider button clicks.

--- jSlider.js
+++ jSlider.js
@@ -7,10 +7,7 @@
 			this.options = $.extend({}, $.jSlider.options, options);
 			this.dispIndex = 0;
 
-			this.queue = 0; //
-
-
-			this.initImages2();
+			this.initImages();
 			this.getImageDimensions();
 			this.initImageFrames();
 			this.frameImages();
@@ -25,13 +22,13 @@
 			this.bindSideButtons();
 
 			this.initNavButtons();
+			this.bindNavButtons();
 			// this.resizeNavButtons();
 
 			this.initAutoScroll();
-			
 		},
 
-		initImages2: function() {
+		initImages: function() {
 			this.images = [];
 			this.shortImages = [];
 			this.firstImageLoaded = new $.Deferred;
@@ -188,7 +185,8 @@
 			this.options.leftBtn.on('click', function() {
 				if(self.slider.queue().length === 0) {
 					// self.start = Date.now();
-					self.disableAutoScroll();
+					self.clearAutoScroll();
+					console.log('autoScroll disabled');
 					// console.log('autoScroll disabled. left');
 				}
 				
@@ -199,8 +197,9 @@
 			this.options.rightBtn.on('click', function() {
 				if(self.slider.queue().length === 0) {
 					// self.start = Date.now();
-					self.disableAutoScroll();
-					console.log('autoScroll disabled. right');
+					self.clearAutoScroll();
+					console.log('autoScroll disabled');
+					// console.log('autoScroll disabled. right');
 				}
 				
 				self.scrollLeft();
@@ -221,30 +220,22 @@
 				this.navButtonsSubContainer.append(newButton);
 			}
 
+			// The height of the buttons are based on the height of its
+			// container which in turn is based on the height of its container, 
+			// which is based on the height of the first/main image.
 			this.firstImageLoaded.done(function() {
 				self.resizeNavButtons();
 				self.activateButton(self.navButtons[self.dispIndex]);
 			})
-			// The height of the buttons is based on the height of its
-			// container which is based on the height of its container, which
-			// is based on the height of the first/main image.
-
-			// var self = this;
-			// for(var time = 0; time < 25; time++) {
-			// 	setTimeout(function(t) {
-			// 		return function() {
-			// 		console.log(t + 'ms:');
-			// 		// console.log(self);
-			// 		console.log(self.navButtons[0][0].getBoundingClientRect().height);
-			// 		// console.log('---')	
-			// 		}
-			// 	}(time), time);
-			// }
+			
 
 			this.navButtonsContainer
 				.append(this.navButtonsSubContainer)
 				.appendTo(this.sliderContainer);
+		},
 
+		bindNavButtons: function() {
+			var self = this;
 			// Add a listener to the container rather than creating
 			// this.images.length listeners.
 			this.navButtonsSubContainer.on('click', function(e) {
@@ -254,7 +245,6 @@
 					clickedIndex = clickedItem.getAttribute('data-btnIndex') || -1;
 
 				if(clickedIndex === -1) { return; }
-				console.log(self.dispIndex + ' ' + clickedIndex);
 				var currentIndex = self.dispIndex,
 					diff = clickedIndex - currentIndex,
 					absDiff = Math.abs(diff);
@@ -272,6 +262,9 @@
 					// Button clicked is to the left of current index.
 				}
 
+				self.clearAutoScroll();
+				console.log('autoScroll disabled');
+
 				if(rightDistance <= leftDistance) {
 					for(var i = 0; i < rightDistance; i++) {
 						self.scrollLeft();
@@ -282,22 +275,7 @@
 						self.scrollRight();
 					}
 				}
-				// // 
-				// if(clickedIndex > currentIndex) {
-				// 	// ScrollLeft
-				// 	for(var index = 0; index < )
-				// } else {
-				// 	// ScrollRight
-				// }
-				
-
 			})
-			// this.dimensionsReady.done(function() {
-			// 	console.log(newButton[0].getBoundingClientRect());
-			// });
-			// console.log(this.navButtons);
-			// console.log(this.navButtonsContainer);
-			// this.
 		},
 
 		resizeNavButtons: function() {
@@ -345,52 +323,19 @@
 
 			if(this.options.autoScroll) {
 				this.scrollHandler = setInterval(function() {
-					if(self.options.autoScroll) { self.scrollLeft(); }
+					self.scrollLeft();
 				}, this.options.scrollInterval);
 			}
 					
 		},
 
-		disableAutoScroll: function() {
-			// console.log(this);
-			this.options.autoScroll = false;
+		clearAutoScroll: function() {
+			this.scrollHandler = clearInterval(this.scrollHandler);
 		},
 
 		enableAutoScroll: function() {
 			this.options.autoScroll = true;
 		},
-
-		// scrollLeft: function() {
-		// 	var self = this,
-		// 		slider = this.slider,
-		// 		css = {'margin-left' : (- this.slider.width() / this.images.length) + 'px'},
-		// 		// Because each 'scroll' adds three items to the queue and
-		// 		// the first item added onto the queue by scrollRight finishes
-		// 		// its execution almost instantaneously.
-		// 		queueLen = Math.ceil(slider.queue().length / 3),
-		// 		dur = this.options.scrollDuration / (queueLen || 1);
-		// 		// console.log(queueLen);
-		// 	slider
-		// 		.animate(css, dur, this.options.easing)
-		// 		.queue(function() { 
-		// 			self.firstToLast()
-		// 				.dequeue();
-		// 			// self.queue--;
-					
-		// 			// console.log('dequeue queueLen:' + (slider.queue().length / 2));//queueLen);
-		// 		})
-		// 		.queue(function() {
-		// 			slider.dequeue();
-		// 			if((slider.queue().length / 3) === 0) { 
-		// 				// console.log('End left:' + (Date.now() - self.start));
-		// 				setTimeout(function() { 
-		// 					self.enableAutoScroll();
-		// 				}, self.options.scrollInterval);
-		// 				console.log('autoScroll enabled. left');
-		// 			}
-		// 		});
-		// 	// console.log('length:' + slider.queue().length);
-		// },
 
 		scrollLeft: function() {
 			var self = this,
@@ -400,8 +345,12 @@
 				// the first item added onto the queue by scrollRight finishes
 				// its execution almost instantaneously.
 				queueLen = Math.ceil(slider.queue().length / 4),
-				dur = this.options.scrollDuration / (queueLen || 1);
+				dur = this.options.scrollDuration / (queueLen + 1);
+				// dur = this.options.scrollDuration / (queueLen || 1);
+				// console.log('qLen:' + queueLen + ' dur:' + dur);
+				// self.total += dur;
 				// console.log(queueLen);
+
 			slider
 				.queue(function() {
 					self.deactivateButton(self.navButtons[self.dispIndex]);
@@ -412,30 +361,29 @@
 					// In order to prevent an array out of bounds error, I had to
 					// do some calculations beforehand.
 					var nextBtnIndex = ((self.dispIndex + 1) > (self.images.length - 1)) ? 0 : (self.dispIndex + 1);
-					// self.activateButton(self.navButtons[self.dispIndex + 1]);
 					self.activateButton(self.navButtons[nextBtnIndex]);
 				})
 				.animate(css, dur, this.options.easing)
 				.queue(function() { 
-					// self.deactivateButton(self.navButtons[self.dispIndex]);
 					self.firstToLast();
-					// self.activateButton(self.navButtons[self.dispIndex]);
 					slider.dequeue();
-					// self.queue--;
-					
-					// console.log('dequeue queueLen:' + (slider.queue().length / 2));//queueLen);
 				})
 				.queue(function() {
 					slider.dequeue();
 					if((slider.queue().length) === 0) { 
 						// console.log('End left:' + (Date.now() - self.start));
-						setTimeout(function() { 
-							self.enableAutoScroll();
-						}, self.options.scrollInterval * 2);
-						// console.log('autoScroll enabled. left');
+						setTimeout(function() {
+							// Check again to make sure no new items were added
+							// to the queue since setTimeout was called.
+							if(/*self.getQueueLength() === 0 && */typeof(self.scrollHandler) === 'undefined') {
+								// self.initAutoScroll();
+								self.initAutoScroll();
+								console.log('autoScroll enabled');
+								// self.clearAutoScroll();
+							}
+						}, self.options.scrollInterval);
 					}
 				});
-			// console.log('length:' + slider.queue().length);
 		},
 
 		firstToLast: function() {
@@ -447,49 +395,6 @@
 			// return this.slider;
 		},
 
-		// scrollRight: function() {
-		// 	var self = this,
-		// 		slider = this.slider,
-		// 		css = { 'margin-left':0 },
-		// 		// Because the first item added to the queue finishes
-		// 		// executing almost instantaneously, I had to use the
-		// 		// ceil function.
-		// 		queueLen = Math.ceil(slider.queue().length / 4),
-		// 		dur = this.options.scrollDuration / (queueLen || 1);
-		// 		console.log(queueLen);
-		// 	slider
-		// 		.queue(function() {
-		// 			self.lastToFirst()
-		// 				.dequeue();
-		// 		})
-		// 		.animate(css, dur, this.options.easing)
-		// 		.queue(function(i) {
-		// 			return function() {
-		// 				// console.log(i);
-		// 				self.activateButton(self.navButtons[i]);
-		// 				slider.dequeue();
-		// 			}
-		// 		}(self.dispIndex))
-		// 		.queue(function() {
-		// 			slider.dequeue();
-		// 			// self.activateButton(self.navButtons[self.dispIndex]);
-		// 			// console.log((slider.queue().length))
-		// 			if((slider.queue().length / 4) === 0) { 
-		// 				// console.log('End right:' + (Date.now() - self.start));
-		// 				setTimeout(function() { 
-		// 					self.enableAutoScroll();
-		// 				}, self.options.scrollInterval);
-		// 				// console.log('autoScroll enabled. right');
-		// 				// console.log(self.dispIndex);
-		// 			}
-		// 			// self.activateButton(self.navButtons[self.dispIndex]);
-		// 		});
-		// 	// Offset the length by 1 to get an even number since the
-		// 	// first item added to the queue finishes its execution
-		// 	// almost instantaneously.
-		// 	// console.log('length:' + (slider.queue().length + 1));
-		// },
-
 		scrollRight: function() {
 			var self = this,
 				slider = this.slider,
@@ -497,9 +402,9 @@
 				// Because the first two items added to the queue finish
 				// executing almost instantaneously, I had to use the
 				// ceil function.
-				queueLen = Math.ceil(slider.queue().length / 4),
+				queueLen = Math.ceil(this.getQueueLength() / 4),
 				dur = this.options.scrollDuration / (queueLen || 1);
-				// console.log(queueLen);
+
 			slider
 				// The first two queues are not combined to have an even
 				// amount of items added to the queue as scrollLeft.
@@ -511,55 +416,20 @@
 					// self.deactivateButton(self.navButtons[self.dispIndex]);
 					self.lastToFirst();
 					self.activateButton(self.navButtons[self.dispIndex]);
-					if(slider.queue().length === 0) {
-						setTimeout (function() {
-							self.enableAutoScroll();
-						}, self.options.scrollInterval);
-					}
 					slider.dequeue();
 				})
 				.animate(css, dur, self.options.easing)
 				.queue(function() {
-					// self.activateButton(self.navButtons[self.dispIndex]);
-					if(slider.queue().length === 0) {
-						setTimeout (function() {
-							self.enableAutoScroll();
-						}, self.options.scrollInterval * 2);
-					}
 					slider.dequeue();
+					if(self.getQueueLength() === 0) {
+						setTimeout (function() {
+							if(/*self.getQueueLength() === 0 && */typeof(self.scrollHandler) === 'undefined') {
+								self.initAutoScroll();
+								console.log('autoScroll enabled');
+							}
+						}, self.options.scrollInterval);
+					}
 				});
-				// .dequeue();
-				// console.log(self.dispIndex);
-				// .queue(function() {
-				// 	self.lastToFirst()
-				// 		.dequeue();
-				// })
-				// .animate(css, dur, this.options.easing)
-				// .queue(function(i) {
-				// 	return function() {
-				// 		// console.log(i);
-				// 		self.activateButton(self.navButtons[i]);
-				// 		slider.dequeue();
-				// 	}
-				// }(self.dispIndex))
-				// .queue(function() {
-				// 	slider.dequeue();
-				// 	// self.activateButton(self.navButtons[self.dispIndex]);
-				// 	// console.log((slider.queue().length))
-				// 	if((slider.queue().length / 4) === 0) { 
-				// 		// console.log('End right:' + (Date.now() - self.start));
-				// 		setTimeout(function() { 
-				// 			self.enableAutoScroll();
-				// 		}, self.options.scrollInterval);
-				// 		// console.log('autoScroll enabled. right');
-				// 		// console.log(self.dispIndex);
-				// 	}
-				// 	// self.activateButton(self.navButtons[self.dispIndex]);
-				// });
-			// Offset the length by 1 to get an even number since the
-			// first item added to the queue finishes its execution
-			// almost instantaneously.
-			// console.log('length:' + (slider.queue().length + 1));
 		},
 
 		lastToFirst: function() {
@@ -576,6 +446,10 @@
 			// return this.slider;
 		},
 
+		getQueueLength: function() {
+			return this.slider.queue().length;
+		},
+
 		activateButton: function($button) {
 			$button.addClass('activated');
 			// console.log($button);
@@ -586,16 +460,17 @@
 			// console.log($button);
 		}
 
-		
-		// TODO: Add bind events
-		// TODO: Dots for navigation (pending)
-		// TODO: IE8 Support (polyfill for Object.create and maybe other stuff).
-		// TODO: Maybe rather than putting the URLs in the script, create img tags in the html instead.
-		
-		// DONE: 'Pause' auto slider when something is clicked until it's done.
-		// DONE: add 'smaller' images to an array and use JS to set their heights and padding and stuff
-		// DONE: Dynamically resize image and add default 'black box' backgrounds
-		// DONE: Add responsive feature
+		/*
+		 * TODO: Add bind events
+		 * TODO: IE8 Support (polyfill for Object.create and maybe other stuff).
+		 * TODO: 'Pause' auto slider when something is clicked until it's done.
+		 * DONE: Dots for navigation (pending)
+		 * DONE: add 'smaller' images to an array and use JS to set their 
+		 * heights and padding and stuff
+		 * DONE: Dynamically resize image and add default 'black box' backgrounds
+		 * DONE: Add responsive feature
+		 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 
 		// Feature: auto "pauses" if user scrolls left or right.  
 	} // end JSlider
@@ -627,41 +502,26 @@
 	// images = ['1.png','grid1.jpg','grid2.jpg','grid3.jpg'];
 	// images = ['5.jpg','6.jpg','7.jpg','8.jpg','9.jpg'];
 	options = {
-		scrollInterval: 3000,
-		autoScroll: false,
+		scrollInterval: 2500,
+		autoScroll: true,
 		buttons: 'auto',
 		width:'800px',
-		scrollDuration: 300
+		scrollDuration: 700
 	}
 	slider1 = $.jSlider(container, images, options);
 	
-	// container2 = $('div.jSlider').eq(1);
-	// // images2 = ['1.png','2.png','3.png','4.png'];
-	// images2 = ['5.jpg','6.jpg','7.jpg','8.jpg','9.jpg'];
-	// options2 = {
-	// 	marginTop: '50px',
-	// 	autoScroll: true,
-	// 	scrollInterval: 2700,
-	// 	scrollDuration: 600,
-	// 	easing: 'swing'
-	// }
-	// slider2 = $.jSlider(container2, images2, options2);
+	container2 = $('div.jSlider').eq(1);
+	// images2 = ['1.png','2.png','3.png','4.png'];
+	images2 = ['5.jpg','6.jpg','7.jpg','8.jpg','9.jpg'];
+	options2 = {
+		marginTop: '50px',
+		autoScroll: true,
+		scrollInterval: 2700,
+		scrollDuration: 600,
+		easing: 'swing'
+	}
+	slider2 = $.jSlider(container2, images2, options2);
 
-	// anchors = $('button');
-	// a = anchors.eq(0);
-	// aContainer = $('div.imgNav > div');
-
-	// var tempHandler;
-	// window.addEventListener('resize', function() {
-	// 	clearTimeout(tempHandler);
-	// 	tempHandler = setTimeout(function() {
-	// 		var temp = a[0].getBoundingClientRect().height;
-	// 		// console.log(temp);	
-	// 		anchors.css('width',temp + 'px');
-	// 		console.log(temp + 'px');
-	// 		aContainer.css('padding-top', ((temp / 0.3) * 0.4) + 'px');
-	// 	}, 0);
-	// })
 })(jQuery);
 
 /*
