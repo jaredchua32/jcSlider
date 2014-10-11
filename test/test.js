b055bb6912d938850eb4a076afc7f4cb89fd8bd0
Jared Chua <jaredchua32@gmail.com>
2014-10-11 00:39:51 -0700

    Updated the locations of the stylesheet and script in the test files. Initial commit for the wiki/images folder.

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
 	images = ['1.png', '2.png', '3.png', '4.png', '1.png', '2.png', '3.png', '4.png', '1.png', '2.png', '3.png', '4.png', '1.png', '2.png', '3.png', '4.png', '1.png', '2.png', '3.png', '4.png'];
 	
 	images.forEach(function(str, index) {
-		images[index] = 'images/' + str;
+		images[index] = '../images/' + str;
 	})
 	// images = ['1.png', 'grid1.jpg', 'grid2.jpg', 'grid3.jpg']
 	options = {
@@ -18,10 +18,10 @@
 		frameBG: 'rgb(255,0,255)',
 		// marginTop: '50px',
 		// resizeDelay: 50,
-		// buttons: 'auto',
+		sideButtons: 'none',
 		// leftBtn: $('div.testLeft'),
-		rightBtn: $('div.testRight'),
-		// navButtons: false,
+		// rightBtn: $('div.testRight'),
+		navButtons: false,
 		// autoScroll: false,
 		// scrollInterval: 1500,
 		// scrollDuration: 600,
@@ -36,16 +36,16 @@
 
 
 
-	container2 = $('div.jSlider').eq(1);
-	// images2 = ['1.png','2.png','3.png','4.png'];
-	images2 = ['images/5.jpg','images/6.jpg','images/7.jpg','images/8.jpg','images/9.jpg'];
-	options2 = {
-		marginTop: '50px',
-		autoScroll: true,
-		scrollInterval: 2700,
-		scrollDuration: 600,
-		easing: 'swing'
-	}
-	slider2 = $.jSlider(container2, images2, options2);
+	// container2 = $('div.jSlider').eq(1);
+	// // images2 = ['1.png','2.png','3.png','4.png'];
+	// images2 = ['images/5.jpg','images/6.jpg','images/7.jpg','images/8.jpg','images/9.jpg'];
+	// options2 = {
+	// 	marginTop: '50px',
+	// 	autoScroll: true,
+	// 	scrollInterval: 2700,
+	// 	scrollDuration: 600,
+	// 	easing: 'swing'
+	// }
+	// slider2 = $.jSlider(container2, images2, options2);
 
 })(jQuery);
