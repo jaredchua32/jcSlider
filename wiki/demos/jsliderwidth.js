19c9d5f30e8730d0c9be36cf5d59f97bcae517b1
Jared Chua <jaredchua32@gmail.com>
2014-10-13 13:41:41 -0700

    Initial commit for jSlider width demo page.

--- wiki/demos/jsliderwidth.js
+++ wiki/demos/jsliderwidth.js
@@ -1,14 +1,26 @@
 ;(function($) {
-	containerOne = $('div#jSliderOne')
-	
-	images = ['5.jpg','6.jpg','7.jpg','8.jpg','9.jpg',]
-	images.forEach(function(str, index) {
-		images[index] = '../../test_images/' + str;
-	})
-
-	optionsOne = {
-		width: 'auto',
+	function firstToLast(inArray) {
+		outArray = inArray;
+		outArray.unshift(inArray[inArray.length - 1]);
+		outArray.pop();
+		return outArray;
 	}
 
-	sliderOne = $.jSlider(containerOne, images, optionsOne);
+	var containerOne = $('div#jSliderOne'),
+		imagesOne = [
+			'../../test_images/5.jpg',
+			'../../test_images/6.jpg',
+			'../../test_images/7.jpg',
+			'../../test_images/8.jpg',
+			'../../test_images/9.jpg'],
+		optionsOne = {
+			'width': '100%',
+			'marginTop': '1em'
+		},
+		
+		containerTwo = $(),
+		imagesTwo = firstToLast(imagesOne),
+
+	sliderOne = $.jSlider(containerOne, imagesOne, optionsOne);
+	sliderTwo = $.jSlider(containerTwo, imagesTwo, optionsOne);
 })(jQuery);
